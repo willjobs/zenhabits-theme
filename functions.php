e8951e9acfb0e361e6893a9c726bbef59ad9eee5
Will <Will@local>
2017-04-20 20:57:04 +0700

    Added og (OpenGraph) tags to header for facebook parsing to show a relevant thumbnail image and description.

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+add_filter( 'jetpack_enable_open_graph', '__return_false' );
+
+
+function get_excerpt_by_id($post_id){
+    $the_post = get_post($post_id); //Gets post ID
+    $the_excerpt = $the_post->post_content; //Gets post_content to be used as a basis for the excerpt
+    $excerpt_length = 35; //Sets excerpt length by word count
+    $the_excerpt = preg_replace( "/\r|\n/", " ", strip_tags(strip_shortcodes($the_excerpt))); //Strips tags and images
+    $words = explode(' ', $the_excerpt, $excerpt_length + 1);
+
+    if(count($words) > $excerpt_length) :
+        array_pop($words);
+        array_push($words, '…');
+        $the_excerpt = implode(' ', $words);
+    endif;
+
+    return $the_excerpt;
+}
+?>
--- header.php
+++ header.php
@@ -8,6 +8,34 @@
 	<meta http-equiv="content-type" 
 		content="text/html;charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=12, minimum-scale=.25, user-scalable=yes" />
+	<meta name="description" content="Blog about adventures and lessons learned while WWOOFing and teaching English in Thailand." />
+	<meta property="og:title" content="<?php echo get_the_title(); ?>" />
+	<meta property="og:type" content="article" />
+	<meta property="og:url" content="<?php echo get_permalink(); ?>" />
+	<meta property="og:site_name" content="<?php echo get_bloginfo('name'); ?>" />
+	<meta property="og:description" content="<?php echo get_excerpt_by_id(get_the_ID()); ?>" />
+
+	<?php
+		$images = get_posts( array(
+		'post_parent'	   => get_the_ID(),
+		'post_type'		 => 'attachment',
+		'numberposts'	   => 1, // Single attachment
+		'post_status'	   => null,
+		'post_mime_type'	=> 'image', // Only images
+		'orderby'		   => 'post_date',
+		'order'			 => 'ASC'
+		) );
+
+		if ( ! empty( $images ) ) {
+			$thumb = wp_get_attachment_image_src( $images[0]->ID, 'thumbnail' );
+			$thumbnail = $thumb[0];
+		} else {
+			$thumbnail = get_site_icon_url();
+		}
+	?>
+
+	<meta property="og:image" content="<?php echo $thumbnail; ?>" />
+	
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
 
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
